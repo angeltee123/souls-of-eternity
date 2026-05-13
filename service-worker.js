518c6f3aaad7386507e30834828abf2f3fdfbba2
angeltee123 <45322441+angeltee123@users.noreply.github.com>
2026-05-12 19:19:10 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,39 @@
-const CACHE_NAME = "souls-of-eternity";
+const CACHE_NAME = "souls-of-eternity-v1";
 
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./game.js",
-  "./manifest.json"
+  "/",
+  "/index.html",
+  "/style.css",
+  "/game.js",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-
   event.waitUntil(
-
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(["./",'.img/icon-192.png']);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", event => {
-
   event.respondWith(
-
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
   );
 });
